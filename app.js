8d4ed4ce57b47cad1428078d84ae037ee7a686ec
Cheedubem <adimonyechidubem@gmail.com>
2023-11-28 15:44:12 +0100

    Created the playGame function which takes the parameters for the player choice and computer choice

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
+// function to get the computer choice
 function getComputerChoice() {
   const options = ["rock", "paper", "scissors"];
   const optionsRandom = Math.floor(Math.random() * options.length);
   return options[optionsRandom];
 }
 
-//Passing The fucntion into an array
-const computerChoice = getComputerChoice();
+// function to get the player choice
+function getPlayerChoice() {
+  const userInput = prompt("Choose one: rock, paper, scissors");
+  return userInput;
+}
 
-console.log(computerChoice);
+// storing both functions in variables
+const computerChoice = getComputerChoice();
+const playerChoice = getPlayerChoice();
+
+// game function
+function playGame(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    console.log("Its a tie!");
+  }
+  elseif(
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+    console.log('You Win!');
+    };
+    else{
+        console.log('You Lose!');
+    }
+    
+}
